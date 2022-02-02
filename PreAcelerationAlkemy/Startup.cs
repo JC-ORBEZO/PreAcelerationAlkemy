27fffae67cd18f0c00309d37074f4e5ad3cb7ff8
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-02-02 12:38:45 -0300

    Cree la cadena de conexion a la base de datos y la agregue a Startup.

--- PreAcelerationAlkemy/Startup.cs
+++ PreAcelerationAlkemy/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PreAcelerationAlkemy.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +30,8 @@ namespace PreAcelerationAlkemy
         {
 
             services.AddControllers();
+            services.AddDbContext<AppDbContext>(options =>
+           options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PreAcelerationAlkemy", Version = "v1" });
