e1c7f947b290929673ed20b500c991a07addadca
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-02-02 12:32:23 -0300

    Cree la carpeta Context y agregue el contexto AppDbContext.

---
+++ PreAcelerationAlkemy/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PreAcelerationAlkemy.Models;
+
+namespace PreAcelerationAlkemy.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
